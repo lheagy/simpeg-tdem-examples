3d3c84cf8a7518f79f5a5ee342d133ee6d7f7408
Lindsey Heagy <lindseyheagy@gmail.com>
2023-03-03 07:28:00 -0800

    add python scripts associated with the notebooks

---
+++ tdem-1d-dbdt-sparse-norms.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1D time domain EM inversion 
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# SimPEG, discretize/
+import discretize
+from discretize import utils
+from SimPEG.electromagnetics import time_domain as tdem
+from SimPEG.electromagnetics import resistivity as dc
+from SimPEG.utils import mkvc, plot_1d_layer_model
+from SimPEG import (
+    maps,
+    data,
+    data_misfit,
+    inverse_problem,
+    regularization,
+    optimization,
+    directives,
+    inversion,
+    utils,
+)
+
+from pymatsolver import Pardiso
+
+
+# ## create a model 
+
+# In[2]:
+
+
+rho_back = 100
+sigma_layer = 0.1
+layer_depth = np.r_[120, 160]
+
+sigma_true = np.r_[1./rho_back, sigma_layer, 1./rho_back]
+m_true = np.log(sigma_true)
+layer_thicknesses_true = np.r_[layer_depth[0], np.diff(layer_depth)]
+
+
+# In[5]:
+
+
+sigma_true
+
+
+# In[4]:
+
+
+fig, ax = plt.subplots(1,1, figsize=(2, 4))
+plot_1d_layer_model(layer_thicknesses_true, sigma_true, ax=ax, label="True")
+ax.grid()
+ax.set_ylim(np.r_[400, 0])
+
+
+# ## set up a survey
+
+# In[7]:
+
+
+tx_radius = 10
+rx_times = np.logspace(-4, -2, 20)
+
+print(f"times: {rx_times.min()*1e3:1.0e}ms, {rx_times.max()*1e3:1.0e}ms")
+
+
+# In[8]:
+
+
+rx_z = tdem.receivers.PointMagneticFluxTimeDerivative(
+    np.r_[0, 0, 0], times=rx_times, orientation="z"
+)
+
+src = tdem.sources.CircularLoop(
+    location=np.r_[0, 0, 0], radius=tx_radius, 
+    receiver_list=[rx_z], #waveform=waveform
+)
+
+survey = tdem.Survey([src])
+
+
+# ## create synthetic data
+
+# In[10]:
+
+
+sim1d_true = tdem.Simulation1DLayered(
+    survey=survey, 
+    thicknesses=layer_thicknesses_true, 
+    sigmaMap=maps.ExpMap(nP=3)
+)
+dobs = sim1d_true.dpred(m_true)
+
+
+# In[11]:
+
+
+fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+                       
+ax.loglog(rx_times*1e3, -dobs, "-o")
+ax.grid()
+ax.set_xlabel("time (ms)")
+ax.set_ylabel("db/dt (V)")
+
+
+# ## set up the inversion
+
+# In[14]:
+
+
+data_invert = data.Data(survey, dobs=dobs, relative_error=0.05)
+data_invert[src, rx_z]
+
+
+# ### create inversion mesh & simulation
+
+# In[17]:
+
+
+cs_invert = 5
+inv_thicknesses = np.hstack([[cs_invert]*int(300/cs_invert), cs_invert * np.logspace(0, 1.5, 20)])
+
+
+# In[18]:
+
+
+mesh = discretize.TensorMesh([(np.r_[inv_thicknesses, inv_thicknesses[-1]])], origin="0")
+mesh
+
+
+# In[19]:
+
+
+sim1d = tdem.Simulation1DLayered(
+    survey=survey, thicknesses=inv_thicknesses, sigmaMap=maps.ExpMap(mesh)
+)
+
+
+# In[20]:
+
+
+def setup_inversion(norms=[0, 0]): 
+    dmis = data_misfit.L2DataMisfit(simulation=sim1d, data=data_invert)
+    reg = regularization.Sparse(
+        mesh, alpha_s=0.1, alpha_x=1.0, 
+        reference_model=np.log(1./rho_back),
+        norms=norms
+    )
+
+    opt = optimization.InexactGaussNewton(maxIter=30)
+    inv_prob = inverse_problem.BaseInvProblem(dmis, reg, opt)
+    
+    # Defining a starting value for the trade-off parameter (beta) between the data
+    # misfit and the regularization.
+    starting_beta = directives.BetaEstimate_ByEig(beta0_ratio=1e1)
+
+    # Options for outputting recovered models and predicted data for each beta.
+    save_iteration = directives.SaveOutputEveryIteration(save_txt=False)
+
+    # Directives for the IRLS
+    update_IRLS = directives.Update_IRLS(
+        max_irls_iterations=10, minGNiter=1, coolEpsFact=1.5, update_beta=True
+    )
+
+    # Updating the preconditionner if it is model dependent.
+    update_jacobi = directives.UpdatePreconditioner()
+
+    # Add sensitivity weights
+    sensitivity_weights = directives.UpdateSensitivityWeights()
+
+    # The directives are defined as a list.
+    directives_list = [
+        sensitivity_weights,
+        starting_beta,
+        save_iteration,
+        update_IRLS,
+        update_jacobi,
+    ]
+    
+    # Here we combine the inverse problem and the set of directives
+    inv = inversion.BaseInversion(inv_prob, directives_list)
+    
+    return inv
+
+
+# In[21]:
+
+
+invl2 = setup_inversion([2, 2])
+starting_model = np.log(1./rho_back)*np.ones(mesh.n_cells)
+
+recovered_model_l2 = invl2.run(starting_model)
+
+
+# In[22]:
+
+
+invl1 = setup_inversion([1, 1])
+recovered_model_l1 = invl1.run(starting_model)
+
+
+# In[23]:
+
+
+invl0 = setup_inversion([0, 0])
+recovered_model_l0 = invl0.run(starting_model)
+
+
+# In[24]:
+
+
+fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+ax.loglog(rx_times*1e3, -dobs, "ko", label="dobs", ms=4)
+ax.loglog(rx_times*1e3, -sim1d.dpred(recovered_model_l2), label="L2")
+ax.loglog(rx_times*1e3, -sim1d.dpred(recovered_model_l1), label="L1")
+ax.loglog(rx_times*1e3, -sim1d.dpred(recovered_model_l0), label="L0")
+
+ax.legend()
+ax.grid("both", alpha=0.7)
+ax.set_xlabel("time (ms)")
+ax.set_ylabel("db/dt")
+
+
+# In[25]:
+
+
+fig, ax = plt.subplots(1, 1, figsize=(3, 5))
+
+plot_1d_layer_model(sim1d_true.thicknesses, np.exp(m_true), ax=ax, color="k", label="True")
+plot_1d_layer_model(mesh.h[0], np.exp(recovered_model_l2), ax=ax, label="L2")
+plot_1d_layer_model(mesh.h[0], np.exp(recovered_model_l1), ax=ax, label="L1")
+plot_1d_layer_model(mesh.h[0], np.exp(recovered_model_l0), ax=ax, label="L0")
+
+ax.set_ylim([450, 0])
+ax.set_xlim([1e-3, 2e-1])
+ax.grid("both", alpha=0.7)
+ax.legend()
+
+ax.set_xlabel("conductivity (S/m)")
+
+
+# In[ ]:
+
+
+
+
